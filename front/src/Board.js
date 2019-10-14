23398003d448abb28362b90f58e2d34a20e67822
Marco Stella <mdstella@gmail.com>
2019-10-14 02:04:01 -0300

    T6: Adding flag on FE

--- front/src/Board.js
+++ front/src/Board.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
 import Row from './Row';
 
 class Board extends Component {
     constructor(props) {
         super(props);
-        this.state = { board: [] };
+        this.state = { board: [], addFlag: false };
     }
 
     componentDidMount() {
@@ -30,19 +35,42 @@ class Board extends Component {
         }
     }
 
+    handleActiveFlagChange = (event) => {
+        let addFlag = this.state.addFlag
+        addFlag = !addFlag
+        this.setState({
+            addFlag: addFlag
+        });
+    }
+
     render() {
         let gameId = this.props.gameId
         let callback = this.cellPickedCallback.bind(this)
+        let addFlag = this.state.addFlag
 
         if (this.state.board !== [] && gameId !== '') {
             // iterates the rows and return the Row component that will render each row
             let rows = this.state.board.map(function (columns, i) {
                 return (
-                    <Row boardCallback={callback} key={i} rowId={i} columns={columns} gameId={gameId} />
+                    <Row boardCallback={callback} key={i} rowId={i} columns={columns} gameId={gameId} addFlag={addFlag} />
                 );
             });
             return (
                 <div>
+                    <FormGroup row>
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={this.state.addFlag}
+                                    onChange={this.handleActiveFlagChange}
+                                    value="checkedB"
+                                    color="primary"
+                                />
+                            }
+                            label="Add flag"
+                        />
+                    </FormGroup>
+
                     <table border="1">
                         <tbody>
                             {rows}
--- front/src/Row.js
+++ front/src/Row.js
@@ -4,20 +4,22 @@ import Cell from './Cell'
 class Row extends Component {
     constructor(props) {
         super(props);
-        this.state = { columns: [] };
+        this.state = { columns: [], addFlag: false };
     }
 
     componentDidMount() {
         this.setState({
             columns: this.props.columns,
+            addFlag: this.props.addFlag
         });
     }
 
     // this will update the state if there are new props on the component
     static getDerivedStateFromProps(props, state) {
-        if (props.columns !== state.columns) {
+        if (props.columns !== state.columns || props.addFlag !== state.addFlag) {
             return {
-                columns: props.columns
+                columns: props.columns,
+                addFlag: props.addFlag
             };
         }
         // Return null to indicate no change to state.
@@ -28,12 +30,20 @@ class Row extends Component {
         let gameId = this.props.gameId
         let rowId = this.props.rowId
         let callback = this.props.boardCallback
+        let addFlag = this.state.addFlag
 
         if (gameId !== '') {
             // Iterates all the columns and with the value it will render each Cell
             let cells = this.state.columns.map(function (cellVal, colId) {
                 return (
-                    <Cell boardCallback={callback} key={colId} colId={colId} rowId={rowId} value={cellVal} gameId={gameId} />
+                    <Cell
+                        boardCallback={callback}
+                        key={colId}
+                        colId={colId}
+                        rowId={rowId}
+                        value={cellVal}
+                        gameId={gameId}
+                        addFlag={addFlag} />
                 );
             });
             return (
